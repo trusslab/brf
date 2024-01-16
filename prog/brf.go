e6cfb979e0530451b39ef9b184174180b65d9f11
Hsin-Wei Hung <hsinweih@uci.edu>
2024-01-15 21:55:42 -0800

    prog: generate a bpf program when generating brf prologue syscalls

--- prog/brf.go
+++ prog/brf.go
@@ -1,5 +1,13 @@
 package prog
 
+import (
+	"fmt"
+	"os/exec"
+	"time"
+
+	"github.com/google/syzkaller/pkg/osutil"
+)
+
 type BpfRuntimeFuzzer struct {
 	isEnabled     bool
 }
@@ -15,3 +23,68 @@ func (brf *BpfRuntimeFuzzer) IsEnabled() bool {
 	return brf.isEnabled
 }
 
+func (brf *BpfRuntimeFuzzer) GenPrologue(r *randGen, s *state, prog *Prog) {
+	var p *BpfProg
+
+	if !brf.isEnabled {
+		return
+	}
+
+	p = brf.genSeedBpfProg(r)
+	_ = p
+}
+
+func (brf *BpfRuntimeFuzzer) genSeedBpfProg(r *randGen) *BpfProg {
+	var opt BrfGenProgOpt
+	var p *BpfProg
+	var ok bool
+
+	opt.useTestSrc = true
+	opt.genProgAttempt = 20
+
+	for i := 0; i < opt.genProgAttempt; i++ {
+		if p, ok = brf.genBpfProg(r, opt); !ok {
+			continue
+		}
+
+		if err := p.writeCSource(); err != nil {
+			fmt.Printf("failed to write bpf program c source: %v\n", err)
+			return nil
+		}
+
+		if err := p.writeGob(); err != nil {
+			fmt.Printf("failed to serialize bpf program: %v\n", err)
+			return nil
+		}
+
+		if err := brf.compileBpfProg(p); err != nil {
+			fmt.Printf("failed to compile bpf program: %v\n", err)
+			continue
+		}
+		return p
+	}
+	return nil
+}
+
+func (brf *BpfRuntimeFuzzer) genBpfProg(r *randGen, opt BrfGenProgOpt) (*BpfProg, bool) {
+	p := newBpfProg(r, opt)
+
+	return p, true
+}
+
+func (brf *BpfRuntimeFuzzer) compileBpfProg(p *BpfProg) error {
+	var timeout time.Duration = 10000000000
+	cmd := exec.Command("clang-16", "-g", "-D__TARGET_ARCH_x86", "-mlittle-endian",
+		"-idirafter", "/usr/local/include",
+		"-idirafter", "/usr/local/llvm/include",
+		"-idirafter", "/usr/include/x86_64-linux-gnu",
+		"-idirafter", "/usr/include",
+		"-Wno-compare-distinct-pointer-types",
+		"-Wno-int-conversion",
+		"-O2", "-target", "bpf", "-mcpu=v3",
+		"-c", p.BasePath + ".c",
+		"-o", p.BasePath + ".o")
+
+	_, err := osutil.Run(timeout, cmd)
+	return err
+}
---
+++ prog/brf_prog.go
@@ -0,0 +1,79 @@
+package prog
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+)
+
+type BpfProg struct {
+	BasePath     string
+	UseTestSrc   bool
+}
+
+type BrfGenProgOpt struct {
+	genProgAttempt   int
+	useTestSrc       bool
+}
+
+func newBpfProg(r *randGen, opt BrfGenProgOpt) *BpfProg {
+	p := &BpfProg {}
+
+	if (opt.useTestSrc) {
+		p.BasePath = fmt.Sprintf("/tmp/test_prog")
+		p.UseTestSrc = true
+	}
+
+	return p
+}
+
+func (p *BpfProg) writeCSource() error {
+	var progSrc string
+
+	if (p.UseTestSrc) {
+		progSrc = testSrc
+	}
+
+	f, err := os.Create(p.BasePath + ".c")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(progSrc)
+	return err
+}
+
+func (p *BpfProg) writeGob() error {
+	f, err := os.Create(p.BasePath + ".gob")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return gob.NewEncoder(f).Encode(p)
+}
+
+var testSrc = `
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+#define DEFINE_BPF_MAP(the_map, TypeOfMap, MapFlags, TypeOfKey, TypeOfValue, MaxEntries) \
+        struct {                                                        \
+            __uint(type, TypeOfMap);                                    \
+            __uint(map_flags, (MapFlags));                              \
+            __uint(max_entries, (MaxEntries));                          \
+            __type(key, TypeOfKey);                                     \
+            __type(value, TypeOfValue);                                 \
+        } the_map SEC(".maps");
+
+DEFINE_BPF_MAP(array_map, BPF_MAP_TYPE_ARRAY, 0, int, int, 1);
+
+SEC("cgroup_skb/egress")
+int func(struct __sk_buff *ctx)
+{
+	int *value, key = 0;
+	value = bpf_map_lookup_elem(&array_map, &key);
+	return 0;
+}
+`
